793f53912f1eac4e65796c99c0c3f89778ce7f68
Nya Elimu <nya@elimu.ai>
2020-06-03 14:06:06 +0800

    Initial commit

---
+++ app/src/main/java/ai/elimu/crowdsource/MainActivity.java
@@ -0,0 +1,17 @@
+package ai.elimu.crowdsource;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(getClass().getName(), "onCreate");
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
