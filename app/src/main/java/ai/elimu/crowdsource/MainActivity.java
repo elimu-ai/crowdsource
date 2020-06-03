cc465b7bf1c614218d48ce2394f909659512127b
Nya Elimu <nya@elimu.ai>
2020-06-03 16:57:53 +0800

    #1 Select mother tongue on first launch
    - Added Spinner

--- app/src/main/java/ai/elimu/crowdsource/MainActivity.java
+++ app/src/main/java/ai/elimu/crowdsource/MainActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import ai.elimu.model.enums.Language;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner languageSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        languageSpinner = findViewById(R.id.language_spinner);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(getClass().getName(), "onStart");
+        super.onStart();
+
+        ArrayAdapter<Language> arrayAdapter = new ArrayAdapter<Language>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                Language.values()
+        );
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(arrayAdapter);
     }
 }
