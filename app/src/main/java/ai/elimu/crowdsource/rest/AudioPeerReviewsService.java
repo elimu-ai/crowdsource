6d7b85a5d78d52f1538d0158c4295670c1903270
Nya <info@elimu.ai>
2021-05-21 10:00:27 +0200

    feat: #9 Peer review AudioContributionEvents

--- app/src/main/java/ai/elimu/crowdsource/rest/AudioPeerReviewsService.java
+++ app/src/main/java/ai/elimu/crowdsource/rest/AudioPeerReviewsService.java
@@ -3,9 +3,12 @@ package ai.elimu.crowdsource.rest;
 import java.util.List;
 
 import ai.elimu.model.v2.gson.crowdsource.AudioContributionEventGson;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 
 /**
  * REST API: https://github.com/elimu-ai/webapp/blob/master/src/main/java/ai/elimu/rest/v2/crowdsource/AudioPeerReviewsRestController.java
@@ -17,4 +20,10 @@ public interface AudioPeerReviewsService {
      */
     @GET("crowdsource/audio-peer-reviews/words")
     Call<List<AudioContributionEventGson>> listWordRecordingsPendingPeerReview(@Header("providerIdGoogle") String providerIdGoogle);
+
+    /**
+     * @param providerIdGoogle The contributor's Google ID.
+     */
+    @POST("crowdsource/audio-peer-reviews/words")
+    Call<ResponseBody> uploadAudioPeerReview(@Header("providerIdGoogle") String providerIdGoogle/*, @Body AudioPeerReviewEventGson audioPeerReviewEventGson*/);
 }
